6e751337bfd18d9c65b0a94dac348b2df7374c4c
Blackburn <flott@Momentum.local>
2016-03-10 17:11:18 -0800

    detwit

---
+++ biff.js
@@ -0,0 +1,13 @@
+a = document.getElementsByClassName('user-actions-follow-button js-follow-btn follow-button btn small small-follow-btn');
+
+window.scrollTo(0,document.body.scrollHeight);
+
+window.onload = function () { window.scrollTo(0,document.body.scrollHeight) }
+
+
+
+
+
+
+
+
